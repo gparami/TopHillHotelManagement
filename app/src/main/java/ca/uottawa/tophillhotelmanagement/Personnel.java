768864a7ebd92a85494b04ac7c0f31f5e29ce48a
Parami Gunasekara <parami@live.ca>
2017-11-29 18:53:24 -0500

    backend class structure

---
+++ app/src/main/java/ca/uottawa/tophillhotelmanagement/Personnel.java
@@ -0,0 +1,13 @@
+package ca.uottawa.tophillhotelmanagement;
+
+/**
+ * Created by parami on 2017-11-29.
+ */
+
+public abstract class Personnel {
+
+    String Name;
+    String email;
+    String password;
+
+}
