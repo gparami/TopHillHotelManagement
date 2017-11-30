bee7c067169f064b08c83f4964cb5c666f1d9a0f
artemtar <artemtarasov89@gmail.com>
2017-11-30 17:59:59 -0500

    new stuff

--- app/src/main/java/ca/uottawa/tophillhotelmanagement/Manager.java
+++ app/src/main/java/ca/uottawa/tophillhotelmanagement/Manager.java
@@ -1,5 +1,7 @@
 package ca.uottawa.tophillhotelmanagement;
 
+import java.util.ArrayList;
+
 /**
  * Created by parami on 2017-11-29.
  */
@@ -7,10 +9,12 @@ package ca.uottawa.tophillhotelmanagement;
 public class Manager extends Personnel {
 
     Department myDepartment;
+    ArrayList<Task> myTasks;
 
-    Manager(String _n, String _email, int _pic, Department d){
+    Manager(String _n, String _email, int _pic, Department d, ArrayList<Task> t){
     super( _n, _email, _pic);
     myDepartment = d;
+    myTasks = t;
     }
 
     public Department getMyDepartment() {
