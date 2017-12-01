3f7fced34a97fc2f7bde003d523bb1d5aa81478b
Parami Gunasekara <parami@live.ca>
2017-12-01 00:42:56 -0500

    fixed tasks, they can be used now.

--- app/src/main/java/ca/uottawa/tophillhotelmanagement/CleaningTask.java
+++ app/src/main/java/ca/uottawa/tophillhotelmanagement/CleaningTask.java
@@ -1,11 +1,43 @@
 package ca.uottawa.tophillhotelmanagement;
 
+import java.util.Date;
+
 /**
- * Created by parami on 2017-11-29.
+ * @Author: parami
+ * @Date: 2017-11-29
+ * @Project: TopHillHotelManagement
  */
 
 public class CleaningTask extends Task {
-    CleaningTask(Priority _p, Personnel _person){
-        super(_p, _person);
+
+    private Room room;
+
+    /**
+     * Creates an unassigned cleaning task specified to a specific room.
+     * @param taskName name of the task.
+     * @param priority priority of the task
+     * @param dueDate due date of the task.
+     * @param room room to be cleaned.
+     */
+    public CleaningTask(String taskName, Priority priority, Date dueDate, Room room) {
+        super(taskName, priority, dueDate);
+        this.room = room;
     }
+
+    /**
+     * Creates an assigned cleaning task specified to a specific room.
+     * @param taskName name of the task.
+     * @param priority priority of the task
+     * @param dueDate due date of the task.
+     * @param room room to be cleaned.
+     * @param assignedTo personnel that the task is assigned to.
+     */
+    public CleaningTask(String taskName, Priority priority, Date dueDate, Room room, Personnel assignedTo) {
+        super(taskName, priority, dueDate, assignedTo);
+        this.room = room;
+    }
+
+    public Room getRoom() { return room; }
+
+    public void setRoom(Room room) { this.room = room; }
 }
---
+++ app/src/main/java/ca/uottawa/tophillhotelmanagement/Priority.java
@@ -0,0 +1,9 @@
+package ca.uottawa.tophillhotelmanagement;
+
+/**
+ * Created by parami on 2017-11-30.
+ *
+ * Used as the enum for tasks.
+ */
+
+public enum Priority { LOW, NORMAL, HIGH }
