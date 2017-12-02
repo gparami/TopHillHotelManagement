455e7b6d01e80b3b10fddbebe212ff3613b39693
Aleks048 <ashum048@uottawa.ca>
2017-12-01 19:06:00 -0500

    With messages

--- app/src/main/java/ca/uottawa/tophillhotelmanagement/Department.java
+++ app/src/main/java/ca/uottawa/tophillhotelmanagement/Department.java
@@ -9,25 +9,11 @@ import java.util.ArrayList;
 public class Department {
 
     private Personnel manager;
+
     public String departmentName;
     public Personnel manager;
-
-    private ArrayList<Personnel> deptEmployees ;
-    private ArrayList<Task> deptTasks;
-
-    public Department(){
-        deptEmployees = new ArrayList<>();
-        deptTasks = new ArrayList<>();
-    }
-
-    public void addTask(Task toAdd){
-        deptTasks.add(toAdd);
-    }
-
-    public ArrayList<Task> getAllTasks(){
-        return new ArrayList<Task>(deptTasks);
-    }
-
+    public ArrayList<Personnel> deptEmployees = new ArrayList<>();
+    public ArrayList<Task> deptTasks = new ArrayList<>();
 
     public Task createTask(){
         return null;
