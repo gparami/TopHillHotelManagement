4500a00eac60faee076bf19ab2c1216605fb73b5
Parami Gunasekara <parami@live.ca>
2017-12-02 21:05:45 -0500

    working receptionist

--- app/src/main/java/ca/uottawa/tophillhotelmanagement/ReceptionistActivity.java
+++ app/src/main/java/ca/uottawa/tophillhotelmanagement/ReceptionistActivity.java
@@ -1,13 +1,115 @@
 package ca.uottawa.tophillhotelmanagement;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.view.MenuItem;
+import android.widget.Toolbar;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Objects;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.firebase.ui.auth.AuthUI;
 
 public class ReceptionistActivity extends AppCompatActivity {
+
+    //Firebase staff is here
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
+    public static int RC_SIGN_IN = 1;//login status
+    public final String ANONYMUS = "ANANIMUS";
+    private String mUsername = ANONYMUS;
+
+
+    public static LinkedList<Personnel> staffList;
+    public static HotelManager hotelManager;
+    public static Manager manager;
+    public static Department currentDepartment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receptionist);
+
+
+        //initialize Firebase staff
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                //check if logined in
+                if (user != null) {
+                    //user is signed in
+                    // Toast.makeText(MainActivity.this, "You are now sighned in.", Toast.LENGTH_SHORT).show();
+                    onSignedInInitialize(user.getDisplayName());
+                } else {
+                    //user is not sighned out
+                    onSignedOutCleanup();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)//log in every time
+                                    .setAvailableProviders(
+                                            Arrays.asList(new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.app_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.logout:
+                AuthUI.getInstance().signOut(this);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    //adding and removing listener if activity is on the background
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
+
+    }
+
+    private void onSignedInInitialize(String user) {
+        mUsername = user;
+        //Build of main activity starts here
+    }
+
+    private void onSignedOutCleanup() {
+        mUsername = ANONYMUS;
+        //destruction before loging out
+    }
+
 }
