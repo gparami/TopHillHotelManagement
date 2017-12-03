76e9e8e8e9a032c0be13262f5e702098943c770f
Taha <tahakhaldi@hotmail.com>
2017-12-03 16:54:42 -0500

    Added Create Task feature for Receptionist

--- app/src/main/java/ca/uottawa/tophillhotelmanagement/Receptionist_CreateTask.java
+++ app/src/main/java/ca/uottawa/tophillhotelmanagement/Receptionist_CreateTask.java
@@ -4,10 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.app.Activity;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 public class Receptionist_CreateTask extends AppCompatActivity {
 
@@ -16,11 +24,11 @@ public class Receptionist_CreateTask extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receptionist__create_task);
 
-        final EditText task_name        = (EditText) findViewById(R.id.task_name);
-        final EditText task_date    = (EditText) findViewById(R.id.task_date);
-        final EditText task_priority     = (EditText) findViewById(R.id.task_priority);
-        final Spinner task_department     = (Spinner) findViewById(R.id.task_department);
-        final Spinner task_assign        = (Spinner) findViewById(R.id.task_assign);
+        final EditText task_name = (EditText) findViewById(R.id.task_name);
+        final EditText task_date = (EditText) findViewById(R.id.task_date);
+        final EditText task_priority = (EditText) findViewById(R.id.task_priority);
+        final Spinner task_department = (Spinner) findViewById(R.id.task_department);
+        final Spinner task_assign = (Spinner) findViewById(R.id.task_assign);
 
         // Create first ArrayAdapter
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
@@ -41,8 +49,9 @@ public class Receptionist_CreateTask extends AppCompatActivity {
             public void onClick(View v) {
 
                 String name      = task_name.getText().toString();
-                String date     = task_date.getText().toString();
-                String priority   = task_priority.getText().toString();
+                String dateTxt     = task_date.getText().toString();
+                String priorityTxt = task_priority.getText().toString();
+                String assignTo = task_assign.getSelectedItem().toString();
 
                 if (TextUtils.isEmpty(name)){
                     task_name.setError("Enter a Task Name");
@@ -50,19 +59,78 @@ public class Receptionist_CreateTask extends AppCompatActivity {
                     return;
                 }
 
-                if (TextUtils.isEmpty(date)){
+                if (TextUtils.isEmpty(dateTxt)){
                     task_date.setError("Enter a Task Date");
                     task_date.requestFocus();
                     return;
                 }
 
-                if (TextUtils.isEmpty(priority)){
+                if (TextUtils.isEmpty(priorityTxt)){
                     task_priority.setError("Enter a Task Priority");
                     task_priority.requestFocus();
                     return;
                 }
+                Toast.makeText(Receptionist_CreateTask.this, "Task Created", Toast.LENGTH_LONG).show();
+
+                finish();
             }
         });
+
     }
+    public Task createTaskObject(){
+
+        final EditText task_name = (EditText) findViewById(R.id.task_name);
+        final EditText task_date = (EditText) findViewById(R.id.task_date);
+        final EditText task_priority = (EditText) findViewById(R.id.task_priority);
+        final Spinner task_department = (Spinner) findViewById(R.id.task_department);
+        final Spinner task_assign = (Spinner) findViewById(R.id.task_assign);
+
+        String name      = task_name.getText().toString();
+        String dateTxt     = task_date.getText().toString();
+        String priorityTxt = task_priority.getText().toString();
+        String assignTo = task_assign.getSelectedItem().toString();
+
+        // String format "2015-08-20";
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date date = null;
+        try
+        {
+            date = sdf.parse(dateTxt);
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        Priority priority = null;
+        if (priorityTxt == "LOW") {
+            priority = Priority.LOW;
+        }
+        else if (priorityTxt == "NORMAL"){
+            priority = Priority.NORMAL;
+        }
+        else if (priorityTxt == "HIGH"){
+            priority = Priority.HIGH;
+        }
+
+        HotelManager myHotelManager = ((Dataset) this.getApplication()).getHotelManager();
+        ArrayList<Personnel> EmployeeList = myHotelManager.staff;
+        Personnel assignedPersonnel = null;
+
+        for(Personnel obj : EmployeeList){
+            if(obj.getName() == assignTo){
+                assignedPersonnel = obj;
+                break;
+            }
+        }
+        Task taskobject;
+        if(assignTo == null){
+            taskobject = new Task(name, priority, date);
+        }
+        else{
+            taskobject = new Task(name, priority, date, assignedPersonnel);
+        }
+        return taskobject;
+    }
+
 }
 
